dca51d916cacb7e9e4d49902705196b79c36be9f
adam-brannon09 <adam.brannon09@gmail.com>
2023-12-12 17:36:12 -0600

    initialized express

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log('Server-side code running');
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+}
+);
+
+
+
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

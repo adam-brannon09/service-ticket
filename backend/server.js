b08811296d89438f8e3a1968693e03186912d9b0
adam-brannon09 <adam.brannon09@gmail.com>
2023-12-17 22:23:28 -0600

    final code change until admin profile is made. deployed to heroku

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,6 @@ app.use(express.json());
 // Middleware to parse the URL-encoded data from the server
 app.use(express.urlencoded({ extended: true }));
 
-// app.get('/', (req, res) => {
-//     res.send('API is running...');
-// }
-// );
 
 // Import the user routes
 app.use('/api/users', require('./routes/userRoutes'));
